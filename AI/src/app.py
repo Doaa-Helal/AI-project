1314ef3d3c706c7f1b5a98b2d3f3651a21022626
Doaa-Helal <doohelal055@gmail.com>
2025-04-26 19:34:10 +0300

    Refactor app.py to improve CORS middleware configuration and formatting

--- AI/src/app.py
+++ AI/src/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import data
 import os
-
-app=FastAPI()
+app = FastAPI()
+app.add_middleware(
+     CORSMiddleware,
+    allow_origins=["http://localhost:3000" , "https://smartsummarizeai.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(data.data_router)
-
-
